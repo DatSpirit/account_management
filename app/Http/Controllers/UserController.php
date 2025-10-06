084567a940c181df916de53cf57f208c975317db
DatSpirit <phanthanhdattin@gmail.com>
2025-10-06 11:23:28 +0700

    (*) Cài đặt giao diện (Scaffolding) Laravel Breeze Blade Stack. Chạy npm install/run dev.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; // sử dụng Auth
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Hiển thị trang cá nhân (profile) của người dùng đã đăng nhập.
+     * Trang này được bảo vệ bởi middleware 'auth'.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        // Lấy đối tượng người dùng hiện tại
+        $user = Auth::user();
+
+        // Trả về view 'user.profile' và truyền dữ liệu $user sang view 
+        return view('user.profile', compact('user'));
+    }
 }
