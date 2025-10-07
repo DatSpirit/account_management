1810e409ffbee5cffb3f779c2385e0dbe979f7f4
DatSpirit <phanthanhdattin@gmail.com>
2025-10-07 10:19:10 +0700

    (+) Define '/admin/users' route for the Admin Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController; // khai báo Controller
+use App\Http\Controllers\UserController; // khai báo Controller cho User
+use App\Http\Controllers\AdminController; // khai báo Controller cho Admin
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,11 +14,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // trang cá nhân User
     Route::get('/profile/user', [UserController::class, 'index'])->name('user.profile');
+// trang quản lý người dùng
+    Route::get('/admin/users', [AdminController::class, 'index'])->name('admin.users');
 });
 
 require __DIR__.'/auth.php';
