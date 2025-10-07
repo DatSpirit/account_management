629b976d8127ceb2ea131ba8d10945e8b7b7d640
DatSpirit <phanthanhdattin@gmail.com>
2025-10-07 13:04:17 +0700

    (+) feat(route): define admin dashboard route in web.php with middleware protection

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+// Trang quản lý người dùng (Admin Dashboard)
+Route::get('/admin/users', [AdminController::class, 'index'])
+    ->middleware(['auth', 'admin'])
+    ->name('admin.users');
+
+// Trang Dashboard chính    
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Các route cần đăng nhập
 Route::middleware('auth')->group(function () {
-    
+    // Trang chỉnh sửa hồ sơ
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// trang cá nhân User
+
+    // trang cá nhân User
     Route::get('/profile/user', [UserController::class, 'index'])->name('user.profile');
-// trang quản lý người dùng
-    Route::get('/admin/users', [AdminController::class, 'index'])
-    ->middleware(['auth', 'admin'])
-    ->name('admin.users');
+
 });
 
 require __DIR__.'/auth.php';
